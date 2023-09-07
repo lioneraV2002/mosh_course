0ba39c43f80b26b980c1eddd0fa701b924546387
Kamyar Maham <kamyar_maham1381@yahoo.com>
2023-09-07 19:05:41 +0330

    creating a new relation model between user and good models

--- app/models.py
+++ app/models.py
@@ -8,14 +8,14 @@ from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.utils import timezone
 from .managers import UserManager
 
-# When creating a custom user model AbstractBaseUser, you must provide fields is_staff and is_active
-
+# When creating a custom user model AbstractBaseUser, you must provide fields is_staff and is_active.
 # to specify that the email field is used for user identification
 # Set USERNAME_FIELD to 'email'.
-
-#  you can define other fields that need to be filled out when creating a user by setting REQUIRED_FIELDS
-
+#  you can define other fields that need to be filled out when creating a user by setting REQUIRED_FIELDS.
 # Finally, Set the objects attribute to the custom manager we created.
+
+import random
+import string
 
 
 
@@ -31,7 +31,8 @@ class User(AbstractBaseUser, PermissionsMixin):
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['first_name', 'last_name']
 
-
+    # create a user manager to make queries to the database and manage users instances
+    # and to call the Manager's methods to create users
     objects = UserManager()
     
     def __str__(self) -> str:
@@ -43,3 +44,19 @@ class Good(models.Model):
     price = models.IntegerField()
 
     
+    
+    
+class UserGoodRelation(models.Model):
+    # many to one relation between relation model and user and good models 
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    good = models.ForeignKey(Good, on_delete=models.CASCADE)
+    count = models.IntegerField()
+
+    def save(self, *args, **kwargs):
+        # Check if the related user and good exist, and create them if not
+        if not self.user_id:
+            password = result_str = ''.join(random.choice(string.ascii_letters) for i in range(9))
+            self.user = User.objects.create_user(email='default@example.com',password=password)
+        if not self.good_id:
+            self.good = Good.objects.create(name='Default Good', price=random.randint(0,99999))
+        super().save(*args, **kwargs)
