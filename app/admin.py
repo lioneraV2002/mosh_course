199abd732c133a9b0a0973054f65280c1197ff07
Kamyar Maham <kamyar_maham1381@yahoo.com>
2023-09-07 17:27:59 +0330

    create form and modify admin config to work with User model

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
+
 # Register your models here.
+# In the admin.py file, 
+# we are configuring the Django admin panel
+# to work with our custom user model
+
+
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth import get_user_model
+
+from .forms import *
+
+User = get_user_model()
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = User
---
+++ app/forms.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth import get_user_model
+
+
+# To view our custom user models in the admin panel
+# we need to create forms.
+
+
+
+User = get_user_model()
+
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = '__all__'
+    
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = User
+        fields = '__all__'
