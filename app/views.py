ae36eea67632e91131de8577bd5793be568e39cd
Kamyar Maham <kamyar_maham1381@yahoo.com>
2023-09-13 00:10:17 +0330

    adding new view methods to register, login, and add goods to users

--- app/views.py
+++ app/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
 from django.http import HttpResponse
+from .forms import *
+
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 # it is a request handler not the view we normally see
@@ -21,3 +25,77 @@ def calculate():
     x = 1
     y = 2
     return x
+
+
+
+
+# registration view method, gets a request,
+# makes a form out of it
+# and does the saving process of the entered information
+# then redirects you to login page
+# if request form is not valid, then, it passes an instance of the form to the render method with register.html file to be rendered 
+# and wait to get the input from data
+
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            # login(request, new_user)
+            return redirect('login')
+    else:
+        form = RegistrationForm()
+    return render(request, 'register.html', {'form': form})
+
+
+
+# login view method, gets a request,
+# makes a form out of it
+# and checks whether it is valid or not.
+# if valid, it logs the user in and redirect them to the add_goods page
+# if not, then, an instance of the form is created, passed to the render method along with login.html file to be rendered with
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            user = authenticate(request, email=email, password=password)
+            if user:
+                login(request, user)
+                return redirect('add_goods')
+    else:
+        form = LoginForm()
+    return render(request, 'login.html', {'form': form})
+
+
+def add_goods(request):
+    if request.method == 'POST':
+        form = AddGoodsForm(request.POST)
+        if form.is_valid():
+            # Get the selected good and count from the form
+            selected_good = form.cleaned_data['good']
+            count = form.cleaned_data['count']
+            
+            # Check if the user already has a UserGoodRelation for the selected good
+            user_good_relation, is_created = UserGoodRelation.objects.get_or_create(user=request.user, good=selected_good)
+            
+            # Update the count for the UserGoodRelation
+            user_good_relation.count += count
+            user_good_relation.save()
+       
+            # Redirect to the add goods page or any other page you prefer
+            return redirect('add_goods')
+    
+    else:
+        form = AddGoodsForm()
+    user_goods = UserGoodRelation.objects.filter(user=request.user)
+    return render(request, 'add_goods.html', {'form': form, 'user_goods': user_goods})
+
+
+
+
+
+def user_logout(request):
+    logout(request)
+    return redirect('login')
