ef301a1896a8666846b71b438045c6eb6d41ee94
Kamyar Maham <kamyar_maham1381@yahoo.com>
2023-09-13 00:09:38 +0330

    adding new urls

--- app/urls.py
+++ app/urls.py
@@ -8,5 +8,10 @@ from . import views
 # URLConf module
 # should include this url conf in the main url configuration
 urlpatterns = [
-    path('hello/', view=views.say_hello)  
+    path('hello/', view=views.say_hello),
+    path('register/', views.register, name='register'),
+    path('login/', views.user_login, name='login'),
+    path('add_goods/', views.add_goods, name='add_goods'),
+    path('logout/', views.user_logout, name='logout'),
+
 ]
